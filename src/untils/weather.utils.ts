4bc9be6b6e08985f769025f92b9c1470bfd479a1
Annie Huang <annie.hn@gmail.com>
2020-08-16 16:05:35 +1000

    Implement logic of convertAPIResponseToDayWeatherList

--- src/untils/weather.utils.ts
+++ src/untils/weather.utils.ts
@@ -1,7 +1,15 @@
 import {dayWeather, dayWeatherAPIResponse} from '../model/weather.model';
 
-export const convertAPIResponseToDayWeatherList = (res: dayWeatherAPIResponse): dayWeather[] => {
-
-  console.log("In convertAPIResponseToDayWeatherList, res=", res);
-  return [];
-}
+export const convertAPIResponseToDayWeatherList = (apiResponses: dayWeatherAPIResponse[]): dayWeather[] => (
+  apiResponses.map(resp => ({
+    precipitation: `${resp.pop}%`,
+    humidity: `${resp.rh}%`,
+    wind: `${(resp.wind_spd * 3.6).toFixed()} kph ${resp.wind_cdir}`, // convert m/s to km/h
+    temperature: resp.temp,
+    weather_description: resp.weather.description,
+    min_temperature: resp.min_temp,
+    max_temperature: resp.max_temp,
+    weather_icon: `/imgs/icons/${resp.weather.icon}.png`,
+    clouds: `${resp.clouds}%`,
+  }))
+)
