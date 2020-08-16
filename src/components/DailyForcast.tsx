7de8d8f96180158269c1f8442d8151a0baf79721
Annie Huang <annie.hn@gmail.com>
2020-08-16 18:46:30 +1000

    Styled the DailyForcast component with boostrap card

--- src/components/DailyForcast.tsx
+++ src/components/DailyForcast.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import {dayWeather} from '../model/weather.model';
 
-const DailyForcast = (day: dayWeather) => {
-  return (
-    <div>
-      {day.precipitation} |
-      {day.humidity} |
-      {day.wind} |
-      {day.temperature} |
-      {day.weather_description} |
-      {day.min_temperature}&#176; |
-      {day.max_temperature}&#176; |
-      <img src={day.weather_icon} alt=""/> |
-      {day.clouds} |
-    </div>
-  );
-};
+/*  <div>
+    {day.precipitation} |
+    {day.humidity} |
+    {day.wind} |
+    {day.temperature} |
+    {day.weather_description} |
+    {day.min_temperature}&#176; |
+    {day.max_temperature}&#176; |
+    <img src={day.weather_icon} alt=""/> |
+    {day.clouds} |
+  </div>*/
+
+const DailyForcast = (day: dayWeather) => (
+  <div className='card text-center'>
+    <h5 style={{marginBottom: 0}}>Wednesday</h5>
+    <img src={day.weather_icon} alt={day.weather_description} />
+    {day.min_temperature}&#176; {day.max_temperature}&#176;<br/>
+    Clouds: {day.clouds}
+  </div>
+);
+
 
 export default DailyForcast;
