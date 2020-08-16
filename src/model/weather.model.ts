d50670c815d0bdffd14f3f0549403784bb03bc10
Annie Huang <annie.hn@gmail.com>
2020-08-16 16:04:49 +1000

    Update weather model to fetch pop instead of precip (pop is the precentage). Also keep all temperature value to number

--- src/model/weather.model.ts
+++ src/model/weather.model.ts
@@ -4,7 +4,7 @@ export interface weather {
 }
 
 export interface dayWeatherAPIResponse {
-  precip: number;
+  pop: number;
   rh: number;
   wind_spd: number;
   wind_cdir: string;
@@ -12,19 +12,20 @@ export interface dayWeatherAPIResponse {
   min_temp: number;
   max_temp: number;
   weather: weather;
+  clouds: number;
 }
 
 export interface dayWeather {
   // use for current day.
-  precipiation: string;         // precip                 e.g. "precip": 0.9375,                          <<< precip: Accumulated liquid equivalent precipitation (default mm)
+  precipitation: string;        // pop                    e.g. "pop": 50,                                 <<< pop: Probability of Precipitation (%)
   humidity: string              // rh                     e.g. "rh": 79,                                  <<< rh: Average relative humidity (%)
   wind: string;                 // wind_spd + wind_cdir   e.g. "wind_spd": 4.05416, | "wind_cdir": "SW",  <<< wind_spd: Wind speed (Default m/s).
-  temperature: string;          // temp                   e.g. "temp": 11.3,                              <<< temp: Temperature (default Celcius).
-  weather_description: string;  // weather.description    e.g. "description": "Broken clouds"            <<< description: Text weather description
+  temperature: number;          // temp                   e.g. "temp": 11.3,                              <<< temp: Temperature (default Celcius).
+  weather_description: string;  // weather.description    e.g. "description": "Broken clouds"             <<< description: Text weather description
 
   // use for forecast days.
-  min_temperature: string       // min_temp               e.g. "min_temp": 8.8,                           <<< min_temp: Minimum Temperature (default Celcius)
-  max_temperature: string       // max_temp               e.g. "max_temp": 14.4,                          <<< max_temp: Maximum Temperature (default Celcius)
+  min_temperature: number       // min_temp               e.g. "min_temp": 8.8,                           <<< min_temp: Minimum Temperature (default Celcius)
+  max_temperature: number       // max_temp               e.g. "max_temp": 14.4,                          <<< max_temp: Maximum Temperature (default Celcius)
 
   // common use for current and forecast days:
   weather_icon: string;         // weather.icon           e.g. "icon": "c03d",                            <<< icon:Weather icon code
