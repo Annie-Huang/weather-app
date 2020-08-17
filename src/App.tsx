13d7a0433fc176821520a3de83e7af53125b9e81
Annie Huang <annie.hn@gmail.com>
2020-08-18 08:46:05 +1000

    Check why the city got reset...

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,23 @@ import SearchWeather from './components/SearchWeather';
 function App() {
   const [city, setCity] = useState('melbourne');
   const [units, setUnits] = useState('M');
-  const {loading, days} = useFetchWeather(city, units);
+  // const {loading, days} = useFetchWeather(city, units);
 
-  console.log('loading=', loading);
-  console.log('days=', days);
+  // console.log('loading=', loading);
+  // console.log('days=', days);
 
   return (
     <div className='container' style={{maxWidth: '950px'}}>
-      <SearchWeather city={city}/>
-      <CurrentWeather {...days[0]} city={city} units={units}/>
-      <div className='forecast-wrapper'>
-        {days.map((day: dayWeather, index: number) => <DailyForecast key={index} {...day}/>)}
-      </div>
+      <SearchWeather city={city} setCity={setCity}/>
+
+      city in App.tsx: {city}
+      {/*<CurrentWeather {...days[0]} city={city} units={units}/>*/}
+      {/*<div className='forecast-wrapper'>*/}
+      {/*  {days.map((day: dayWeather, index: number) => <DailyForecast key={index} {...day}/>)}*/}
+      {/*</div>*/}
+
+      <button className="btn btn-primary" onClick={() => setCity('annie')}>Search</button>
+
     </div>
   );
 }
--- src/components/SearchWeather.tsx
+++ src/components/SearchWeather.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import './SearchWeather.css';
 
-const SearchWeather = ({city: string}) => {
+const SearchWeather = ({city, setCity}) => {
+  const [searchCity, setSearchCity] = useState(city);
+
+  const handleSubmitData = () => {
+    console.log('annieannie, searchCity=', searchCity);
+    setCity(searchCity);
+  }
+
   return (
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlInput1" className="form-label">Search weather for your city</label>
-      <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-    </div>
+    <form className='my-3'>
+      <label htmlFor="city" className="form-label">Search Weather for your city</label>
+      <input type="text" className="form-control" id="city" name="city" value={searchCity} onChange={(e) => setSearchCity(e.target.value)}/>
+      city: {city}
+      <br />
+
+      Search city: {searchCity}
+      <br />
+
+      {/*<div className="form-check mt-2">*/}
+      {/*  <input className="form-check-input" type="radio" name="weatherUnit" id="Celsius" checked/>*/}
+      {/*    <label className="form-check-label" htmlFor="Celsius">*/}
+      {/*      Celsius*/}
+      {/*    </label>*/}
+      {/*</div>*/}
+      {/*<div className="form-check mb-2">*/}
+      {/*  <input className="form-check-input" type="radio" name="weatherUnit" id="Fahrenheit" />*/}
+      {/*    <label className="form-check-label" htmlFor="Fahrenheit">*/}
+      {/*      Fahrenheit*/}
+      {/*    </label>*/}
+      {/*</div>*/}
+
+      {/*<button type="submit" className="btn btn-primary" onClick={handleSubmitData}>Search</button>*/}
+      {/*<button className="btn btn-primary" onClick={handleSubmitData}>Search</button>*/}
+      <button className="btn btn-primary" onClick={() => setCity(searchCity)}>Search</button>
+    </form>
   );
 };
 
 export default SearchWeather;
+
+/*
+<input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example">
+<input class="form-control" type="text" placeholder="Default input" aria-label="deafult input example">
+<input class="form-control form-control-sm" type="text" placeholder=".form-control-sm" aria-label=".form-control-sm example">
+ */
+
