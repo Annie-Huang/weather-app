7a48ad5d368aa72682a6b51d10be8b4a547d4b7b
Annie Huang <annie.hn@gmail.com>
2020-08-22 14:58:21 +1000

    Calls to setUnits directly when user switching between Celsius and Fahrenheit rather than waiting for button submission

--- src/components/SearchWeather.tsx
+++ src/components/SearchWeather.tsx
@@ -7,27 +7,21 @@ interface ISearchWeatherProp {
   city: string;
   setCity: (searchedCity: string)=> void;
   units: string;
-  setUnits: (searchedUnits: string)=> void;
+  setUnits: (units: string)=> void;
 }
 
 const SearchWeather: React.FC<ISearchWeatherProp> = ({city, setCity, units, setUnits}) => {
   const [searchedCity, setSearchedCity] = useState(city);
-  const [searchedUnits, setSearchedUnits] = useState(units);
-
-  const handleSubmitData = () => {
-    setCity(searchedCity);
-    setUnits(searchedUnits);
-  }
 
   return (
     <form className='my-3'>
       <label htmlFor="city" className="form-label">Search Weather for your city</label>
       <input type="text" className="form-control mb-2" id="city" name="city" aria-label="search city" value={searchedCity} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchedCity(e.target.value)}/>
-      <button type='button' className="btn btn-primary" onClick={handleSubmitData}>Search</button>
+      <button type='button' className="btn btn-primary" onClick={()=>setCity(searchedCity)}>Search</button>
 
       {WEATHER_UNIT_NAME_MAPPER.map((item, index) =>
         <div className="form-check mt-2" key={index}>
-          <input className="form-check-input" type="radio" name="weatherUnit" id={item.name} onChange={() => setSearchedUnits(item.id)} checked={searchedUnits === item.id}/>
+          <input className="form-check-input" type="radio" name="weatherUnit" id={item.name} onChange={() => setUnits(item.id)} checked={units === item.id}/>
           <label className="form-check-label" htmlFor={item.name}>
             {item.name}
           </label>
