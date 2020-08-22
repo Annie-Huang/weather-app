6458e5678064346b4376b0cedd8c19455df7b8ea
Annie Huang <annie.hn@gmail.com>
2020-08-22 16:17:06 +1000

    Styled the SearchWeather component

--- src/components/SearchWeather.tsx
+++ src/components/SearchWeather.tsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
-import './SearchWeather.css';
 import {WEATHER_UNIT_NAME_MAPPER} from '../constant/weather.constant';
-
 
 interface ISearchWeatherProp {
   city: string;
@@ -14,19 +12,21 @@ const SearchWeather: React.FC<ISearchWeatherProp> = ({city, setCity, units, setU
   const [searchedCity, setSearchedCity] = useState(city);
 
   return (
-    <form className='my-3'>
+    <form className='my-3 d-flex flex-column justify-content-center'>
       <label htmlFor="city" className="form-label">Search Weather for your city</label>
       <input type="text" className="form-control mb-2" id="city" name="city" aria-label="search city" value={searchedCity} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchedCity(e.target.value)}/>
       <button type='button' className="btn btn-primary" onClick={()=>setCity(searchedCity)}>Search</button>
 
-      {WEATHER_UNIT_NAME_MAPPER.map((item, index) =>
-        <div className="form-check mt-2" key={index}>
-          <input className="form-check-input" type="radio" name="weatherUnit" id={item.name} onChange={() => setUnits(item.id)} checked={units === item.id}/>
-          <label className="form-check-label" htmlFor={item.name}>
-            {item.name}
-          </label>
-        </div>
-      )}
+      <div className="d-flex justify-content-center">
+        {WEATHER_UNIT_NAME_MAPPER.map((item, index) =>
+          <div className="form-check mt-2 mr-4" key={index}>
+            <input className="form-check-input" type="radio" name="weatherUnit" id={item.name} onChange={() => setUnits(item.id)} checked={units === item.id}/>
+            <label className="form-check-label" htmlFor={item.name}>
+              {item.name}
+            </label>
+          </div>
+        )}
+      </div>
     </form>
   );
 };
