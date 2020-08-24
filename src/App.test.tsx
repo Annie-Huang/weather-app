4ac92f0f8d20756f6ed0094471b4915595adb56e
Annie Huang <annie.hn@gmail.com>
2020-08-24 21:52:58 +1000

    Give up on proper test on App.tsx. Cannot get useRedux and useEffect and axio and container component hook up for test. Make a back up on the test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+// @ts-ignore
+import { mount} from 'enzyme';
+import SearchWeather from './components/SearchWeather';
+import axios from 'axios';
+import { act } from 'react-dom/test-utils';
+import {resp} from './sampleResponse.ts';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+
+it('fetches successfully data from an API', async () => {
+  await act(async () => {
+    axios.get.mockImplementationOnce(() => Promise.resolve(resp));
+    // axios.get.mockImplementationOnce(() => Promise.reject(new Error('Network Error')),);
+
+    const wrapper = mount(<App />)
+
+    wrapper.update();
+    // wrapper.forceUpdate();
+    // wrapper.instance().forceUpdate();
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find(SearchWeather)).toHaveLength(1);
+  });
 });
